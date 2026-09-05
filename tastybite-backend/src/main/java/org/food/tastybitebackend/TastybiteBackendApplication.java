50d98dbd9b6013ac66ad495df6c7e973a0ccc6aa
GOPINATH <ngopi6769@gmail.com>
2026-09-05 22:46:47 +0530

    tastybite-v1-001

---
+++ tastybite-backend/src/main/java/org/food/tastybitebackend/TastybiteBackendApplication.java
@@ -0,0 +1,13 @@
+package org.food.tastybitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastybiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastybiteBackendApplication.class, args);
+    }
+
+}
---
+++ tastybite-backend/src/test/java/org/food/tastybitebackend/TastybiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.food.tastybitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastybiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
